685c9365f536324abde11b1e870ed3d33f1ef550
Daniele Bolla <bolladaniele@gmail.com>
2025-04-04 14:00:31 +0100

    init show T conn but not path conn

--- keeptrack.lean
+++ keeptrack.lean
@@ -1,4 +1,11 @@
 import Mathlib
+--Defining each set
+
+def S : Set (ℝ × ℝ) := (fun x ↦ (x, Real.sin (x⁻¹))) '' Set.Ioi (0 : ℝ)
+
+def Z : Set (ℝ × ℝ) := { (0, 0) }
+
+def T : Set (ℝ × ℝ) := S ∪ Z
 
 lemma invFunIsContinuousOnPosReal : ContinuousOn (fun x : ℝ => x⁻¹) (Set.Ioi (0 : ℝ)) := by
   apply ContinuousOn.inv₀
@@ -9,17 +16,11 @@ lemma invFunIsContinuousOnPosReal : ContinuousOn (fun x : ℝ => x⁻¹) (Set.Io
 lemma sinWithinvFunIsContinuousOnPosReal : ContinuousOn (fun x : ℝ => Real.sin (x⁻¹)) (Set.Ioi (0 : ℝ)) := by
   apply Real.continuous_sin.continuousOn.comp
   · exact invFunIsContinuousOnPosReal
-  · intro x hx -- getting soe wierd type when checking goal
+  · intro x hx -- getting some wierd type when checking goal
     exact Set.mem_of_subset_of_mem (fun ⦃a⦄ a ↦ trivial) rfl -- i don't understand what's going on here (proved with exact?)
 
 lemma topoSinCurveIsContinuousOnPosReal : ContinuousOn (fun x ↦ (x, Real.sin (x⁻¹))) (Set.Ioi (0 : ℝ)) :=
   ContinuousOn.prod continuous_id.continuousOn sinWithinvFunIsContinuousOnPosReal
-
-def S : Set (ℝ × ℝ) := (fun x ↦ (x, Real.sin (x⁻¹))) '' Set.Ioi (0 : ℝ)
-
-def Z : Set (ℝ × ℝ) := { (0, 0) }
-
-def U : Set (ℝ × ℝ) := S ∪ Z
 
 lemma posIntervalIsPathConnected : IsPathConnected (Set.Ioi (0 : ℝ)) := by
   apply Convex.isPathConnected
@@ -33,17 +34,25 @@ lemma SIsPathConn : IsPathConnected S := by
   · exact topoSinCurveIsContinuousOnPosReal
 
 -- Connectedness
-lemma SIsConnected : IsConnected S := SIsPathConn.isConnected
+lemma SisConnected : IsConnected S := SIsPathConn.isConnected
 
-lemma ZIsConnected : IsConnected Z := isConnected_singleton
+lemma ZisConnected : IsConnected Z := isConnected_singleton
 
-lemma UIsConnected : IsConnected U := by
-  apply IsConnected.union
-  · use (0, 0)
-    constructor
-    · sorry
-    · rfl
-  · exact SIsConnected
-  · exact ZIsConnected
+lemma TisNotPathConn : ¬ (IsPathConnected T)  := by
+  -- unfold IsPathConnected
+  by_contra h
+  unfold IsPathConnected at h
+  -- obtain ⟨U, h, j⟩ := h
+  sorry
 
--- Need To Prove U not path Connected
+lemma TIsConnected : IsPreconnected T := by
+    unfold IsPreconnected
+    by_contra h
+    push_neg at h
+    obtain ⟨U, V, hU, hV, hUnion, hDisj, hSep⟩ := h
+
+    have SisPreconnected : IsPreconnected S := SisConnected.isPreconnected
+    rw[IsPreconnected] at SisPreconnected
+    let U' := U ∩ S
+    let V' := V ∩ S
+    sorry
